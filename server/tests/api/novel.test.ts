5ebc1aa8769da74b4653631441b3dbe9321c38da
Ringo37 <yajima.haruhiko@gmail.com>
2024-06-29 06:42:44 +0900

    add test for novel

--- server/tests/api/novel.test.ts
+++ server/tests/api/novel.test.ts
@@ -4,13 +4,12 @@ import { GET, POST } from './utils';
 
 test(GET(noCookieClient.novel), async () => {
   const res = await noCookieClient.novel.$get();
-
   expect(res).toEqual('Hello');
 });
 
 test(POST(noCookieClient.novel), async () => {
-  const aozoraUrl = 'abc';
+  const aozoraUrl=noCookieClient.novel.$path()
   const res = await noCookieClient.novel.$post({ body: { aozoraUrl } });
 
-  expect(res).toEqual(aozoraUrl);
+  expect(typeof res).toEqual('string');
 });
